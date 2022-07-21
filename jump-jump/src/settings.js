47df62fc63cd331a0c6598120a6127b75fdc110f
sushantGitH <69434965+sushantGitH@users.noreply.github.com>
2022-07-21 15:24:29 +0530

    jump

---
+++ jump-jump/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","group1"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/GameScene.fire",orientation:"portrait",jsList:[]};
